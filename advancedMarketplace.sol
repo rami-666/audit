e2df0696f619cb098feedeaee5996473ab335ad1
rami-666 <ramihamadeh2002@gmail.com>
2023-01-24 17:51:20 +0200

    minor fixes

--- advancedMarketplace.sol
+++ advancedMarketplace.sol
@@ -584,7 +584,8 @@ contract advancedMarketplace is
     }
 
     function setArtistFee(address _lazyAddress, uint256 percentage) external {
-        require(lazyNft(_lazyAddress).hasRole(0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6, msg.sender), "Marketplace: can't set fee if you are not an artist");
+        require(lazyNft(_lazyAddress).hasRole(0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6, msg.sender), "Marketplace: can't set fee if you are not an artist.");
+        require(percentage <= 10, "Marketplace: set percentage exceeds limit.");
         artistFee[msg.sender] = percentage;
     }
 
